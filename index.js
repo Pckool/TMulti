d5b74c1bf85e958d6e95b49e27997ec413be8181
Philippe <phillclesco@yahoo.com>
2019-03-07 13:17:20 -0500

    new file:   .gitignore 	modified:   index.js 	modified:   package.json 	modified:   shrinkwrap.yaml 	new file:   src/authed.html 	modified:   src/css/master.css 	modified:   src/index.html 	modified:   src/js/home.js 	modified:   src/streams.html

--- index.js
+++ index.js
@@ -4,36 +4,79 @@ const fs = require('fs');
 var app = express();
 const axios = require('axios');
 
-var TEmitter = require(`${__diename}/my_modules/TEmitter`);
+var TEmitter = require(`${__dirname}/my_modules/TEmitter`);
 
+var tmi = require('tmi.js');
 
 app.use(express.static(`${__dirname}/src`));
 const port = 8008;
 
 var client_id = 'zu9oogpw8m51xccq8g0nuwkpln4c5f';
+var redirectURI = 'http://multi.multi.localhost:8008/login';
+
+
+const opts = {
+  identity: {
+    username: "",
+    password: "",
+  },
+  channels: []
+};
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   console.log(`subdomains: ${req.subdomains}`);
   if (req.subdomains[0] === 'multi' && req.subdomains.length === 1){
     console.log('this is the multi subdomain...');
-    var streamer = req.params.streamer;
     res.sendFile(`${__dirname}/src/index.html`);
   }
 
 });
 
 app.get('/login', (req, res) => {
-  var access_token = req.query.access_token;
-  axios.post('https://api.twitch.tv/helix/validate', {
-    Authorization: `Bearer ${access_token}`,
-    "Client-ID": client_id
-  })
-  .then((response) => {
-    response
-  })
-  .catch((err) => {
-    res.send('There was a problem with the token. Please try again.')
-  });
+  let code = req.query.code;
+  let client_secret="f56g35tnxhl89o6dr2x3tuq4e5zbyp";
+  // let token = req.query.code;
+  console.log(code);
+
+  if(code){
+    // OAuth Token
+    axios({
+      method: 'post',
+      url: `https://id.twitch.tv/oauth2/token`,
+      params: {
+        "client_id": client_id,
+        "client_secret": "f56g35tnxhl89o6dr2x3tuq4e5zbyp",
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": redirectURI
+      }
+    })
+    .then((response) => {
+      // response.data
+      res.sendFile(`${__dirname}/src/authed.html`);
+    })
+    .catch((err) => {
+      res.send('There was a problem with the token. Please try again.\n' + err);
+    });
+  }
+  else{
+
+  }
 });
 
+app.get('/streams', (req, res) => {
+  if (req.subdomains[0] === 'multi' && req.subdomains.length === 1){
+    res.sendFile(`${__dirname}/src/streams.html`);
+  }
+
+});
 app.listen(port, () => {console.log(`connected via port ${port}.`)})
--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,49 @@
 var redirectURI = 'http://multi.multi.localhost:8008/login';
 var scope = 'chat:read+chat:edit+channel:moderate';
 var client = 'zu9oogpw8m51xccq8g0nuwkpln4c5f';
-$(".butn-twitch").on('click', () => {
-  window.open(`https://id.twitch.tv/oauth2/authorize?client_id=${client}&redirect_uri=${redirectURI}&response_type=token&scope=${scope}&force_verify=true`);
+$("#loginTwitch").on('click', () => {
+  window.location = `https://id.twitch.tv/oauth2/authorize?client_id=${client}&redirect_uri=${redirectURI}&response_type=code&scope=${scope}&force_verify=true`;
 
 });
+
+$("#startMulti").on('click', () => {
+  var streamer_1 = $("#streamer-1").val().toLowerCase();
+  var streamer_2 = $("#streamer-2").val().toLowerCase();
+
+  $('<div>', {
+    class: "channel-1"
+  }).appendTo('.streams-find');
+  $('<div>', {
+    class: "channel-2"
+  }).appendTo('.streams-find');
+
+  $('<iframe>', {
+    src: `https://player.twitch.tv/?volume=1&muted&channel=${streamer_1}`,
+    id:  'streamer-1-player',
+    class: "stream-player",
+    frameborder: 0,
+    scrolling: 'no'
+  }).appendTo('.channel-1');
+  $('<iframe>', {
+    src: `https://www.twitch.tv/embed/${streamer_1}/chat?darkpopout`,
+    id:  'streamer-1-chat',
+    frameborder: 0,
+    scrolling: 'no'
+  }).appendTo('.channel-1');
+
+  $('<iframe>', {
+    src: `https://player.twitch.tv/?volume=1&muted&channel=${streamer_2}`,
+    id:  'streamer-2-player',
+    class: "stream-player",
+    frameborder: 0,
+    scrolling: 'no'
+    }).appendTo('.channel-2');
+    $('<iframe>', {
+      src: `https://www.twitch.tv/embed/${streamer_2}/chat?darkpopout`,
+      id:  'streamer-2-chat',
+      frameborder: 0,
+      scrolling: 'no'
+    }).appendTo('.channel-2');
+});
+// `https://player.twitch.tv/?volume=1&muted&channel=${streamer_1}`
+// `https://www.twitch.tv/popout/${streamer_2}/chat?popout=`
